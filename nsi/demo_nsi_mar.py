c7005d401251c35748b1a31a4fe115d68bbe0a97
Diyu Yang <yang1467@gilbreth-g003.rcac.purdue.edu>
2024-07-24 15:40:15 -0400

    Change the sequence of image display

--- nsi/demo_nsi_mar.py
+++ nsi/demo_nsi_mar.py
@@ -120,6 +120,6 @@ if __name__ == "__main__":
     vmin = 0
     vmax = downsample_factor[0]*0.008
 
-    pu.slice_viewer(init_recon, recon_mar, vmin=0, vmax=vmax, slice_axis=0, slice_label='Axial Slice', title='recon with transmission_root weight (left) VS recon with MAR weight (right)')
-    pu.slice_viewer(init_recon, recon_mar, vmin=0, vmax=vmax, slice_axis=1, slice_label='Coronal Slice', title='recon with transmission_root weight (left) VS recon with MAR weight (right)')
     pu.slice_viewer(init_recon, recon_mar, vmin=0, vmax=vmax, slice_axis=2, slice_label='Sagittal Slice', title='recon with transmission_root weight (left) VS recon with MAR weight (right)')
+    pu.slice_viewer(init_recon, recon_mar, vmin=0, vmax=vmax, slice_axis=1, slice_label='Coronal Slice', title='recon with transmission_root weight (left) VS recon with MAR weight (right)')
+    pu.slice_viewer(init_recon, recon_mar, vmin=0, vmax=vmax, slice_axis=0, slice_label='Axial Slice', title='recon with transmission_root weight (left) VS recon with MAR weight (right)')
