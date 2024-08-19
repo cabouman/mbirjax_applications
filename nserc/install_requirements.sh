f840cce7c2e36f868ea062ac302684687af347ec
Diyu Yang <yang1467@gilbreth-k009.rcac.purdue.edu>
2024-08-18 20:47:44 -0400

    add script to install nserc demo dependencies

---
+++ nserc/install_requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script installs the dependencies for the nserc demo
+# It is assumed that mbirjax has already been installed
+conda install tomopy
+conda install conda-forge::dxchange
