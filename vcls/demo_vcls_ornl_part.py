983c5f3018b15e1d6ab9ffc449504fc53111aa8b
Charles Bouman <charles.bouman@gmail.com>
2025-06-09 08:41:24 -0400

    Buggy version of ornl scipt with correct flow

--- vcls/demo_vcls_ornl_part.py
+++ vcls/demo_vcls_ornl_part.py
@@ -24,10 +24,6 @@ if __name__ == '__main__':
     # Set VCLS parameters
     #####################
     num_selected_views = 40
-    # r_1 = voxel sampling rate in (0,1]. Smaller => faster; Larger => more accurate
-    r_1 = 0.002
-    # r_2 = view sampling rate used for stochastic search in (0,1]. Smaller => faster; Larger => more accurate
-    r_2 = 0.5
 
     ######################
     # Set recon parameters
@@ -59,33 +55,23 @@ if __name__ == '__main__':
         if f.lower().endswith(('.h5', '.hdf5'))
     )
     filename = os.path.join(dataset_dir_scan, hdf5_files[0])
-    full_sinogram, cone_beam_params_for_recon, optional_params_for_recon = out.compute_sino_and_params(filename)
-    angle_candidates = cone_beam_params_for_recon['angles']  # This is probably not the best way to do this
-
-    # Construct different params used in VCLS
-    cone_beam_params_for_vcls = cone_beam_params_for_recon.copy()
-    optional_params_for_vcls = optional_params_for_recon.copy()
-    num_views = len(angle_candidates)
-    num_det_rows = reference_object.shape[2]
-    num_det_channels = reference_object.shape[0]
-    sinogram_shape_for_vcls = (num_views, num_det_rows, num_det_channels)
-    cone_beam_params_for_vcls["sinogram_shape"] = sinogram_shape_for_vcls
+    full_sino, cone_beam_params, optional_params = out.compute_sino_and_params(filename)
+    angle_candidates = cone_beam_params['angles']  # This is probably not the best way to do this
 
     # Construct cone beam object using ORNL parameters
-    ct_model_for_vcls = mj.ConeBeamModel(**cone_beam_params_for_vcls)
-
-    # Set optional ORNL geometry parameters
-    ct_model_for_vcls.set_params(**optional_params_for_vcls)
+    ct_model = mj.ConeBeamModel(**cone_beam_params)
+    # Set optional geometry parameters
+    ct_model.set_params(**optional_params)
 
     ##############################################
     # Run VCLS to Select Views and Display Results
     ##############################################
     time0 = time.time()
-    optimal_angle_inds, vcl_value = mjp.get_opt_views(ct_model_for_vcls, reference_object, num_selected_views, r_1=r_1, r_2=r_2, verbose=1, seed=seed)
+    optimal_angle_inds, vcl_value = mjp.get_opt_views(ct_model, reference_object, num_selected_views, verbose=1, seed=seed)
     optimal_angles = angle_candidates[optimal_angle_inds]
     elapsed = time.time() - time0
-    print('Elapsed time for selected views is {:.3f} seconds'.format(elapsed))
-    print('VCL value for selected views: {:.6f}'.format(vcl_value))
+    print('Elapsed time for VCLS view selection is {:.3f} seconds'.format(elapsed))
+    print('VCL value for selected views = {:.6f}'.format(vcl_value))
 
     # Display reference object cross-section with selected angles
     formatted = np.array2string(optimal_angles, precision=3, suppress_small=True, separator=', ')
@@ -98,34 +84,28 @@ if __name__ == '__main__':
     angles_perp = optimal_angles + np.pi / 2    # Add 90deg because Fourier transform of edge is perpendicular to edge
     mjp.show_image_with_projection_rays(np.log10(1e-2 + np.abs(ref_fft)), rotation_angles_rad=angles_perp, title='FFT of Reference Object\n with Selected View Angles')
 
-
-    # Construct cone beam object for recon using ORNL parameters
-    ct_model_for_recon = mj.ConeBeamModel(**cone_beam_params_for_recon)
-    # Set optional ORNL geometry parameters
-    ct_model_for_recon.set_params(**optional_params_for_recon)
-    # Set recon parameters
-    ct_model_for_recon.set_params(sharpness=sharpness, snr_db=snr_db)
-
-    # Do a recon with optimal angles
+    # Compute mbir recon with optimal angles
     optimal_angles = angle_candidates[optimal_angle_inds]
-    ct_model_opt = mjp.copy_ct_model(ct_model_for_recon, optimal_angles)
-    sinogram_optimal_angles = full_sinogram[optimal_angle_inds]
-    recon_optimal_angles, recon_params = ct_model_opt.recon(sinogram_optimal_angles, max_iterations=max_iterations)
+    ct_model_opt = mjp.copy_ct_model(ct_model, optimal_angles)
+    sinogram_opt = full_sino[optimal_angle_inds]
+    recon_opt, recon_params = ct_model_opt.recon(sinogram_opt)
 
-    # Do a recon with uniform angles
-    # Find uniform sampled index list
-    num_det_channels_for_recon = cone_beam_params_for_recon["sinogram_shape"][2]
-    source_detector_dist = cone_beam_params_for_recon["source_detector_dist"]
+    # Compute detector cone angle
+    num_det_channels_for_recon = cone_beam_params["sinogram_shape"][2]
+    source_detector_dist = cone_beam_params["source_detector_dist"]
     detector_cone_angle = 2 * np.arctan2(num_det_channels_for_recon / 2, source_detector_dist)
+
+    # Compute uniform sampling angles for a short scan
     candidates_normalized = np.abs(angle_candidates - angle_candidates[0])
     end_index = np.where(candidates_normalized < np.pi + detector_cone_angle)[0][-1] # final angle in the short-scan range
     uniform_index_list = dut.create_uniform_index(angle_candidates, end_index, num_selected_views)
     uniform_angles = angle_candidates[uniform_index_list]
 
-    ct_model_uniform = mjp.copy_ct_model(ct_model_for_recon, uniform_angles)
-    sinogram_uniform = full_sinogram[uniform_index_list]
-    recon_uniform, recon_params_uniform = ct_model_uniform.recon(sinogram_uniform, max_iterations=max_iterations)
+    # Compute mbir recon for uniformly sampled short scan
+    ct_model_uniform = mjp.copy_ct_model(ct_model, uniform_angles)
+    sino_uniform = full_sino[uniform_index_list]
+    recon_uniform, recon_params_uniform = ct_model_uniform.recon(sino_uniform, max_iterations=max_iterations)
 
-    mj.slice_viewer(recon_uniform, recon_optimal_angles, slice_label=['Uniform: Slice', 'VCLS optimal: Slice'],
+    mj.slice_viewer(recon_uniform, recon_opt, slice_label=['Uniform: Slice', 'VCLS optimal: Slice'],
                     title='Recons from {} views: \nuniformly spaced angles (left) and optimal angles (right)'.format(num_selected_views), vmin=0.0, vmax=0.05)
 
