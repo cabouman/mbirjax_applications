f8c5e2fa99ea76a4efafcb264b4af29c37cd35a3
gbuzzard <buzzard@purdue.edu>
2025-09-07 20:34:50 -0400

    Print elapsed time even if display_comparison is False.

--- nsi/demo_split_sino_recon.py
+++ nsi/demo_split_sino_recon.py
@@ -68,8 +68,9 @@ if __name__ == "__main__":
     recon, recon_dict = ct_model.split_sino_recon(sino)  # weights can be passed as third arg if available
     t1 = time.time()
 
+    print(f"Stitched recon shape: {recon.shape}   (elapsed: {t1 - t0:.1f}s)")
+
     if display_comparison:
-        print(f"Stitched recon shape: {recon.shape}   (elapsed: {t1 - t0:.1f}s)")
         mj.slice_viewer(recon, recon_std, data_dicts=[recon_dict, recon_dict_std], slice_axis=1, title="Split Sino Recon (left) vs Standard Recon (right)")
     else:
         mj.slice_viewer(recon, data_dicts=recon_dict, slice_axis=1, title="Split Sino Recon")
