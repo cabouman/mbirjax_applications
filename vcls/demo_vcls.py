77d8e3cecac0e52bd3cc89b4b9712ecf8abf8377
Jingsong Lin <jl3872@cornell.edu>
2026-03-23 21:45:46 -0400

    Update VCLS demo to support new input parameters

--- vcls/demo_vcls.py
+++ vcls/demo_vcls.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
     # This can be useful in real application when a small number of views is measured first.
     inital_number_of_views = 2
     prev_selected_view_inds, vcl_value = mj.get_opt_views(ct_model, reference_object, inital_number_of_views, r_1=r_1, r_2=r_2, verbose=1, seed=seed)
-    prev_selected_angles = angle_candidates[prev_selected_angle_inds]
+    prev_selected_angles = angle_candidates[prev_selected_view_inds]
 
     # The select the remaining num_selected_views-2 views
     optimal_angle_inds, vcl_value = mj.get_opt_views(ct_model, reference_object, num_selected_views - inital_number_of_views, r_1=r_1, r_2=r_2, prev_selected_view_inds=prev_selected_view_inds, priority_order=priority_order, verbose=1, seed=seed)
