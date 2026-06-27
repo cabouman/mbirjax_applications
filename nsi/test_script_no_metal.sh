ebb93040d8d56bc6895e8461ad764086d097837d
Greg Buzzard <buzzard@purdue.edu>
2026-06-27 11:53:12 -0400

    Add no metal test script with little downsampling.

---
+++ nsi/test_script_no_metal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python Lilly_recon.py \
+  --data_path /depot/bouman/data/Lilly/Autoinjector_HighRes_Horizontal/ \
+  --downsampling 1 \
+  --subsample_view_factor 2 \
+  --num_metal 0 \
+  --sino_cropping 1
+
+
+
