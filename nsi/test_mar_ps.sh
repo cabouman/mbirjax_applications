aa1102dc781e149112f7a70c68060b15ef44a79c
ZiyunLiiii <ziyunliiii@gmail.com>
2026-07-10 22:15:58 -0500

    Save log file after recon

--- nsi/test_mar_ps.sh
+++ nsi/test_mar_ps.sh
@@ -12,4 +12,5 @@ python Lilly_recon_ps.py \
   --num_metal 2 \
   --sino_cropping 1 \
   --partition_sequence skip_0 \
-  --max_iterations 15
+  --max_iterations 15 \
+  2>&1 | tee ~/mbirjax_notes/Lilly_mar_run.log
